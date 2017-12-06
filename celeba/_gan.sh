447bad6a52e7d3731e686463aac9f85e578ae86c
Cameron Fabbri <cameronfabbri@gmail.com>
2017-12-06 10:10:15 -0600

    update

--- celeba/_gan.sh
+++ celeba/_gan.sh
@@ -1,1 +1,1 @@
-python celeba_cgan.py --LOSS=gan --DATA_DIR=/mnt/data1/images/celeba/ --EPOCHS=100 --BATCH_SIZE=64
+python celeba_cgan.py --LOSS=gan --DATA_DIR=/mnt/data1/images/celeba/ --EPOCHS=25 --BATCH_SIZE=64
